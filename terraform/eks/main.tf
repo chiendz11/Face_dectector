258b20b6b11fdc7db6be76411b6a4497afc94593
chiendz11 <thuha25121976@gmail.com>
2026-04-27 23:08:16 +0700

    fix(terraform): stabilize EKS Kubernetes providers

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -152,29 +152,27 @@ resource "aws_autoscaling_group_tag" "cluster_autoscaler_cluster" {
   }
 }
 
-data "aws_eks_cluster" "this" {
-  name = module.eks.cluster_name
-
-  depends_on = [module.eks]
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
-
-  depends_on = [module.eks]
-}
-
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
   }
 }
 
